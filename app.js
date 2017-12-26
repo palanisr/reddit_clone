0f660465ace75c2493eae27828f5f0114cb45c5b
Ruban Palanipillai <s.r.palanipillai@gmail.com>
2017-12-26 14:27:47 -0500

    added initial files for starting point

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-(function () {
+/*(function () {
     angular.module('app', [])
         .component('reddit', {
             controller: function () {
@@ -6,4 +6,33 @@
             },
             templateUrl: "reddit.html"
         })
-})()
+})()*/
+
+var app= angular.module('app', []);
+
+//Directive
+
+var myDirective = function(){
+  return{
+
+          templateUrl: "reddit.html"
+      };
+}
+
+//Controller
+
+var myController = function($scope){
+    var a=$scope;
+
+    a.tab=false;
+    a.setTab = function(state){
+        console.log('Inside function');
+        a.tab=state;
+    }
+
+}
+
+app.directive('reddit',myDirective);
+app.controller('MainController',myController);
+
+
